89e521aac4eef5fcb1b0fa04daff9c61fbe791d9
Stephane Bres <stephanejrbres@gmail.com>
2015-07-29 17:52:19 +0800

    add login

--- login.py
+++ login.py
@@ -32,7 +32,27 @@ def hello_world():
 
 @application.route('/login', methods=['POST'])
 def login():
-    pass
+    try:
+        client = aerospike.client(config).connect()
+    except:
+        logging.error("failed to connect to the cluster with // {0}".format(config['hosts']))
+        return 'KO', 500
+    to_check = ['username', 'password']
+    for element in to_check:
+        if request.form.get(element) is None:
+            return '{0} not in request.'.format(element)
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    (key, meta, bin) = client.get((namespace, 'user', username))
+    if meta is None:
+        return 'User {0} don\'t exists.'.format(username), 422
+    h_password = hashlib.sha512(password).hexdigest()
+    db_pass = bin['password']
+    if db_pass != h_password:
+        return 'Wrong password', 401
+    _return = {'mail': bin['mail']}
+    return _return
 
 
 @application.route('/singin', methods=['POST'])
@@ -53,7 +73,7 @@ def inscription():
     (key, meta) = client.exists((namespace, 'user', username))
     if meta is not None:
         return 'User {0} already exists.'.format(username), 422
-    uid = uuid4()
+    uid = str(uuid4())
     h_password = hashlib.sha512(password).hexdigest()
     bin = {'id': uid,
            'password': h_password,
